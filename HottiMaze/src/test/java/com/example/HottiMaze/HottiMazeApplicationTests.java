bb80d9561b82ece3d88d6550a10f4f2667cb142d
sungchan12 <chanyouunng1@gmail.com>
2025-05-30 15:37:53 +0900

    Add properties

---
+++ HottiMaze/src/test/java/com/example/HottiMaze/HottiMazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HottiMaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HottiMazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/example/HottiMaze/HottiMazeApplication.java
@@ -0,0 +1,13 @@
+package com.example.HottiMaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HottiMazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HottiMazeApplication.class, args);
+	}
+
+}
