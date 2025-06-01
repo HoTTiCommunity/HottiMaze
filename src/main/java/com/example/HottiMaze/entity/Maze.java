ae873681dfa14b80bb68716635fbcac5d23d66f8
sungchan12 <chanyouunng1@gmail.com>
2025-06-01 14:11:33 +0900

    add imgs and Maze Structure

--- src/main/java/com/example/HottiMaze/entity/Maze.java
+++ src/main/java/com/example/HottiMaze/entity/Maze.java
@@ -1,15 +1,24 @@
 package com.example.HottiMaze.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Maze {
     @Id
     private Long id;
-    @Column(name = "answer")
-    private String answer;
-    @Column(name = "MazeDir")
+    @Column(name = "MazeDir", nullable = false)
     private String MazeDir;
+    @Column(name = "MazeTitle", nullable = false)
+    private String MazeTitle;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @Column(name = "view_count")
+    private Integer viewCount = 0;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
